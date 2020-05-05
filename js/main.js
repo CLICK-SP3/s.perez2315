e62e5ace41a771a7c0d5359ea98be9056796a142
CLICK-SP3 <click_sp3@yahoo.com>
2020-05-04 21:50:19 -0500

    Module8_jQuery branch
    Update May 4th.

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $('#dismiss').click(function(){       
+    // console.log('This is my Saturday2 JavaScript');
+    $('#jumbotron').fadeOut(3000);
+    }); 
+        setTimeout(function(){
+            $('#shift').fadeIn('bigcopy');
+        },5000)
+
+    // setTimeout(function () {
+    //     $('#title').addClass('red');
+    //     }, 2000);
+        
+    //     })
+});
+
+// $(document).ready(function () {
+//     $('#dismiss').click(function(){       
+//     // console.log('This is my Saturday2 JavaScript');
+//     $('#jumbotron').fadeOut(2500, function() {
+//         $('#shift').addClass('bigcopy');
+//         // console.log('Damn this stuff actually works');
+//     });
+//     // $('#shift').addClass('bigcopy');
+//     }) 
+// });
+
+
+// .hide ()
+
+// .show()
+
+// .click(function()){
+
+// })
+    // $('#shift').addClass('bigcopy');
+
